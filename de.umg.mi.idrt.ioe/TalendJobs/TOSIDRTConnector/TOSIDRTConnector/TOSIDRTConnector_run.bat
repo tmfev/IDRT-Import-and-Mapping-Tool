1ca2c16be8e9d64121f3936c2bea4f66bf1e5ef1
christian.bauer@med.uni-goettingen.de <christian.bauer@med.uni-goettingen.de>
2013-10-08 12:32:12 +0200

    deleting unnecessary TOS files IDRT-10 IDRT-13

--- de.umg.mi.idrt.ioe/TalendJobs/TOSIDRTConnector/TOSIDRTConnector/TOSIDRTConnector_run.bat
+++
@@ -1,3 +0,0 @@
-%~d0
- cd %~dp0
-java -Xms256M -Xmx1024M -cp classpath.jar; tos.tosidrtconnector_0_1.TOSIDRTConnector --context=Default %* 
--- de.umg.mi.idrt.ioe/TalendJobs/TOSIDRTConnector/TOSIDRTConnector/TOSIDRTConnector_run.sh
+++
@@ -1,3 +0,0 @@
-cd `dirname $0`
- ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -cp classpath.jar: tos.tosidrtconnector_0_1.TOSIDRTConnector --context=Default "$@" 
