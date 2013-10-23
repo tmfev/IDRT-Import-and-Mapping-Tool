daf1fea3b52d7b968d820c419411637ded101652
benjamin.baum@med.uni-goettingen.de <benjamin.baum@med.uni-goettingen.de>
2013-10-23 17:29:11 +0200

    Fixes DeleteNode for OntologySubTreeNode

--- de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/DeleteNodeCommand.java
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/DeleteNodeCommand.java
@@ -11,6 +11,7 @@ import de.umg.mi.idrt.ioe.OntologyTree.MyOntologyTree;
 import de.umg.mi.idrt.ioe.OntologyTree.OntologyTree;
 import de.umg.mi.idrt.ioe.OntologyTree.OntologyTreeModel;
 import de.umg.mi.idrt.ioe.OntologyTree.OntologyTreeNode;
+import de.umg.mi.idrt.ioe.OntologyTree.OntologyTreeSubNode;
 import de.umg.mi.idrt.ioe.view.EditorTargetInfoView;
 import de.umg.mi.idrt.ioe.view.OntologyEditorView;
 
@@ -22,27 +23,35 @@ public class DeleteNodeCommand extends AbstractHandler {
 		TreeViewer targetTreeViewer = OntologyEditorView.getTargetTreeViewer();
 		IStructuredSelection selection = (IStructuredSelection) targetTreeViewer
 				.getSelection();
-		OntologyTreeNode firstElement = (OntologyTreeNode) selection
-				.getFirstElement();
-		OntologyEditorView.setNotYetSaved(true);
-		if (!(firstElement ==myOT.getSubRootNode())) {
 
-			firstElement.removeFromParent();
+		if (selection.getFirstElement() instanceof OntologyTreeNode) {
+			OntologyTreeNode firstElement = (OntologyTreeNode) selection
+					.getFirstElement();
+			OntologyEditorView.setNotYetSaved(true);
+			if (!(firstElement ==myOT.getSubRootNode())) {
+
+				firstElement.removeFromParent();
+			}
+			else {
+				firstElement.removeFromParent();
+				firstElement.getChildren().clear();
+				myOT.getOntologyTreeTarget()
+				.getNodeLists().add(firstElement);
+				//			editorTargetView.setComposite(OTTarget);
+				//TODO Ontology Editor
+				//			firstElement.removeFromParent();
+				for (OntologyTreeNode node : firstElement.getChildren()) {
+					firstElement.remove(node);
+				}
+			}
 		}
-		else {
-			firstElement.removeFromParent();
-			firstElement.getChildren().clear();
-			myOT.getOntologyTreeTarget()
-			.getNodeLists().add(firstElement);
-			//			editorTargetView.setComposite(OTTarget);
-			//TODO Ontology Editor
-			//			firstElement.removeFromParent();
-						for (OntologyTreeNode node : firstElement.getChildren()) {
-							firstElement.remove(node);
-						}
+		else if (selection.getFirstElement() instanceof OntologyTreeSubNode) {
+			OntologyTreeSubNode subNode = (OntologyTreeSubNode) selection.getFirstElement();
+			System.err.println("NYI");
 		}
 		targetTreeViewer.refresh();
 		return null;
+
 	}
 
 }
