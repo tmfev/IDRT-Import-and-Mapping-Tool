71e4ed67a1958742d4a2edb56b2016a29786a1ae
christian.bauer@med.uni-goettingen.de <christian.bauer@med.uni-goettingen.de>
2013-10-28 10:37:32 +0100

    merge with nothing

---
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/OntologyTree/OntologyTreeSubNode.java
@@ -0,0 +1,93 @@
+package de.umg.mi.idrt.ioe.OntologyTree;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import de.umg.mi.idrt.ioe.view.OntologyEditorView;
+
+/**
+ * @author Benjamin Baum <benjamin(dot)baum(at)med(dot)uni-goettingen(dot)de>
+ * Department of Medical Informatics Goettingen
+ * www.mi.med.uni-goettingen.de
+ */
+
+public class OntologyTreeSubNode extends DefaultMutableTreeNode{
+	private static final long serialVersionUID = 1L;
+	private OntologyTreeNode parent;
+	private String stagingPath;
+	private String stagingName;
+	private boolean highlighted;
+	private OntologyTreeNode stagingParentNode;
+
+	public OntologyTreeSubNode(OntologyTreeNode parent) {
+		
+		if (parent != null) {
+			if (parent.getTargetNodeAttributes().getSourcePath() != null)
+				this.setStagingPath(parent.getTargetNodeAttributes().getSourcePath());
+		
+		this.parent = parent;
+		}
+		else {
+			System.err.println("Parent null");
+		}
+	}
+
+	/**
+	 * @return the parent
+	 */
+	public OntologyTreeNode getParent() {
+		return parent;
+	}
+
+	/**
+	 * @param parent the parent to set
+	 */
+	public void setParent(OntologyTreeNode parent) {
+		this.parent = parent;
+	}
+
+	public String getStagingPath() {
+		return stagingPath;
+	}
+
+	public void setStagingPath(String sourcePath) {
+		this.stagingPath = sourcePath;
+		if (OntologyEditorView.getI2b2ImportTool()!=null) {
+			OntologyTreeNode parentNode = OntologyEditorView.getI2b2ImportTool().getMyOntologyTrees().getOntologyTreeSource().getNodeLists().getNodeByPath(sourcePath);
+			if (parentNode == null) {
+				parentNode = OntologyEditorView.getI2b2ImportTool().getMyOntologyTrees().getOntologyTreeTarget().getNodeLists().getNodeByPath(sourcePath);
+			}
+			this.setStagingParentNode(parentNode);
+		}
+	}
+
+	public String getStagingName() {
+		return stagingName;
+	}
+
+	public void setStagingName(String stagingName) {
+		this.stagingName = stagingName;
+	}
+
+	/**
+	 * @param b
+	 */
+	public void setHighlighted(boolean b) {
+		this.highlighted = b;
+
+	}
+	/**
+	 * 
+	 */
+	public boolean isHighlighted() {
+		// TODO Auto-generated method stub
+		return this.highlighted;
+	}
+
+	public OntologyTreeNode getStagingParentNode() {
+		return stagingParentNode;
+	}
+
+	public void setStagingParentNode(OntologyTreeNode stagingParentNode) {
+		this.stagingParentNode = stagingParentNode;
+	}
+
+}
