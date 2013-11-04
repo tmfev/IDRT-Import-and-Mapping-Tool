56e89f321e85a5f008475f4c5bfc934bb2713d19
christian.bauer@med.uni-goettingen.de <christian.bauer@med.uni-goettingen.de>
2013-11-04 18:21:13 +0100

    deleting old functions

--- de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/GUITools.java
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/GUITools.java
@@ -18,51 +18,6 @@ import de.umg.mi.idrt.ioe.OntologyTree.MyOntologyTree;
 public class GUITools {
 	
 
-	/**
-	 * Creates an image icon.
-	 *
-	 * @param image the name of the image file
-	 * @return the new image icon
-	 */	
-	public static ImageIcon createImageIcon(String image) {
-		
-		
-		
-		java.net.URL imgURL = MyOntologyTree.class.getResource(//"images/" +
-				image);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL);
-		} else {
-			System.err.println("Couldn't find file: " + image);
-			return null;
-		}
-		
-	}
-	
-	public static Image createImage(String imageName) {
-		
-		ImageDescriptor imageDesc = AbstractUIPlugin.imageDescriptorFromPlugin(
-				"de.umg.mi.idrt.ioe",
-				imageName);
-		
-		
-		
-		Image image = null;
-		
-		if (imageDesc != null){
-			image = imageDesc.createImage();
-		}
-		
-				
-
-		if (image != null) {
-			return image;
-		} else {
-			System.err.println("Couldn't find file: " + image + " (" + imageName + ")");
-			return null;
-		}
-		
-	}
 	
 	/**
 	 * Get an image by image name.
