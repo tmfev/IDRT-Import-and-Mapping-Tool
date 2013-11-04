e6a0ef210039fd8d7b8cd78b7f8e2e8fccd8adec
christian.bauer@med.uni-goettingen.de <christian.bauer@med.uni-goettingen.de>
2013-11-04 17:56:14 +0100

    merge

---
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/OntologyTree/Dimension.java
@@ -0,0 +1,22 @@
+package de.umg.mi.idrt.ioe.OntologyTree;
+
+/**
+ * @author Christian Bauer
+ *         <christian(dot)bauer(at)med(dot)uni-goettingen(dot)de> Department of
+ *         Medical Informatics Goettingen www.mi.med.uni-goettingen.de
+ * 
+ *         
+  */
+
+public enum Dimension {
+	
+	/**
+	 * the i2b2 concept_dimension
+	 */
+	CONCEPT_DIMENSION,
+	/**
+	 * the i2b2 modifier_dimension
+	 */
+	MODIFIER_DIMENSION
+	
+}
---
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/LoadTargetOntology.java
@@ -0,0 +1,73 @@
+package de.umg.mi.idrt.ioe.commands.OntologyEditor;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import de.umg.mi.idrt.ioe.ActionCommand;
+import de.umg.mi.idrt.ioe.Application;
+import de.umg.mi.idrt.ioe.Console;
+import de.umg.mi.idrt.ioe.Resource;
+import de.umg.mi.idrt.ioe.OntologyTree.OntologyTreeTargetRootNode;
+import de.umg.mi.idrt.ioe.OntologyTree.TOSConnector;
+import de.umg.mi.idrt.ioe.OntologyTree.Target;
+import de.umg.mi.idrt.ioe.OntologyTree.TargetProject;
+import de.umg.mi.idrt.ioe.OntologyTree.TargetProjects;
+import de.umg.mi.idrt.ioe.view.OntologyEditorView;
+
+
+public class LoadTargetOntology extends AbstractHandler {
+
+	
+	public LoadTargetOntology(){
+		super();
+	}
+
+
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+	
+
+		Console.info("Command: LoadTargetOntology");
+		
+		TOSConnector tos = new TOSConnector();
+		
+		TargetProjects targetProjects = ((OntologyTreeTargetRootNode)OntologyEditorView.getI2b2ImportTool().getMyOntologyTrees().getOntologyTreeTarget().getTreeRoot()).getTargetProjects();
+
+		Target target = null;
+		
+		if (targetProjects.getSelectedTarget() == null || targetProjects.getSelectedTargetProject() == null ){
+			Console.error("Can not load target ontology, because no target is selected.");
+			return null;
+		}
+		
+		target = targetProjects.getSelectedTarget();
+		
+	
+
+		try {
+
+			tos.setContextVariable("Job", "LoadTargetOntology");
+			tos.setContextVariable("TargetID", String.valueOf(target.getTargetID()));
+			//tos.setContextVariable("SQLTable", "I2B2");
+	
+			tos.runJob();
+			
+			
+
+		} catch (Exception e) {
+			String message = "Error while using a TOS-plugin for job \"LoadTargetOntology\": "
+					+ e.getMessage();
+			Console.error(message);
+			Application.getStatusView().addErrorMessage(message);
+
+
+		}
+		
+		
+		
+		return null;
+	}
+}
