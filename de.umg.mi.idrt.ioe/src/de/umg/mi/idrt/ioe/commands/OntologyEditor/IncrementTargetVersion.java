a5cb145e7d8c05291e62d18884f10d24817c4702
christian.bauer@med.uni-goettingen.de <christian.bauer@med.uni-goettingen.de>
2013-11-08 13:48:58 +0100

    removed the automatic saving of the old version ontology

--- de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/IncrementTargetVersion.java
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/IncrementTargetVersion.java
@@ -17,63 +17,36 @@ public class IncrementTargetVersion extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		System.out.println("Command: IncrementTargetVersion");
-		
 		TargetProjects targetProjects = OntologyEditorView.getTargetProjects();
-
-		
-		System.out.println("------------------------------");
-		System.out.println("------- save target");
-		
-		System.out.println("-/1------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
-		
-		
+				
 		// save the old target project version
+		/*
 		if ( OntologyEditorView.isNotYetSaved() ) {
 			ActionCommand command0  = new ActionCommand(Resource.ID.Command.IEO.SAVETARGETPROJECT);
 			Application.executeCommand(command0);
 		}
-		System.out.println("-/2------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
-		
+		*/
 		
 		// save ontology as the old target version
 		ActionCommand command1  = new ActionCommand(Resource.ID.Command.IEO.SAVETARGET);
 		Application.executeCommand(command1);
-		
-		System.out.println("-/3------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
-		
-		
+				
 		Target newTarget = targetProjects.incrementVersion(targetProjects.getSelectedTarget());
 		
 		targetProjects.setSelectedTarget(newTarget);
-		
-		System.out.println("-/B------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
 		
 		
 		// save the new target project version
 		ActionCommand command2  = new ActionCommand(Resource.ID.Command.IEO.SAVETARGETPROJECT);
 		Application.executeCommand(command2);
 		
-		System.out.println("-/4------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
 		
 		
-		
-		
-		System.out.println("-/5------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
-		
+			
 		
 		// save ontology as the new target version
 		ActionCommand command3  = new ActionCommand(Resource.ID.Command.IEO.SAVETARGET);
 		Application.executeCommand(command3);
-		
-		System.out.println("-/6------ targetProjectID - targetID - Version |");
-		System.out.println("--------- " + targetProjects.getSelectedTarget().getTargetProjectID() + "               - " + targetProjects.getSelectedTarget().getTargetID() + "        - " + targetProjects.getSelectedTarget().getVersion() + "       |");
 		
 		Application.getStatusView().addMessage(
 				"A new version of this i2b2 staging project has been created.");
