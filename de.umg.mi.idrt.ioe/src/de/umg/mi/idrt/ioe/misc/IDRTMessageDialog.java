9d277dcf3f69d35fce4394eaa4da4efee5192192
benjamin.baum@med.uni-goettingen.de <benjamin.baum@med.uni-goettingen.de>
2013-12-09 09:25:35 +0100

    Adds Custom Message Dialog.

---
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/misc/IDRTMessageDialog.java
@@ -0,0 +1,41 @@
+package de.umg.mi.idrt.ioe.misc;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author Benjamin Baum <benjamin(dot)baum(at)med(dot)uni-goettingen(dot)de>
+ * Department of Medical Informatics Goettingen
+ * www.mi.med.uni-goettingen.de
+ */
+public class IDRTMessageDialog extends MessageDialog {
+
+	/**
+	 * @param parentShell
+	 * @param dialogTitle
+	 * @param dialogTitleImage
+	 * @param dialogMessage
+	 * @param dialogImageType
+	 * @param dialogButtonLabels
+	 * @param defaultIndex
+	 */
+	public IDRTMessageDialog(Shell parentShell, String dialogTitle, Image dialogTitleImage, String dialogMessage, int dialogImageType,
+			String[] dialogButtonLabels, int defaultIndex) {
+		super(parentShell, dialogTitle, dialogTitleImage, dialogMessage, dialogImageType, dialogButtonLabels, defaultIndex);
+	}
+
+	public static boolean openIDRTWarning(Shell parent, String title, String message) {
+		return open(CONFIRM, parent, title, message, SWT.NONE);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.MessageDialog#open()
+	 */
+	@Override
+	public int open() {
+		// TODO Auto-generated method stub
+		return super.open();
+	}
+}
