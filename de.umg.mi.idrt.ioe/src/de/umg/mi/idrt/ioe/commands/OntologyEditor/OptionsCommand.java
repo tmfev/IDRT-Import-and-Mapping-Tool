a661945d54ec0f3e3fb27e7d95954f35d31bffbe
benjamin.baum@med.uni-goettingen.de <benjamin.baum@med.uni-goettingen.de>
2014-02-20 13:52:27 +0100

    Fixes Options.

---
+++ de.umg.mi.idrt.ioe/src/de/umg/mi/idrt/ioe/commands/OntologyEditor/OptionsCommand.java
@@ -0,0 +1,27 @@
+package de.umg.mi.idrt.ioe.commands.OntologyEditor;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+
+import de.umg.mi.idrt.ioe.Application;
+import de.umg.mi.idrt.ioe.wizards.OptionsWizard;
+
+/**
+ * @author Benjamin Baum <benjamin(dot)baum(at)med(dot)uni-goettingen(dot)de>
+ *         Department of Medical Informatics Goettingen
+ *         www.mi.med.uni-goettingen.de
+ */
+public class OptionsCommand extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		WizardDialog wizardDialog = new WizardDialog(Application.getShell(),
+				new OptionsWizard());
+		wizardDialog.open();
+		return null;
+
+	}
+}
